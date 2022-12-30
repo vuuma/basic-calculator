7bc22722de58e1189d0aed460c27a1452fad7dd0
Matthew <matthewvuu@gmail.com>
2022-12-30 18:23:01 +1100

    create functions for basic math operators

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+function add(firstInteger, secondInteger){
+    return firstInteger + secondInteger;
+}
+
+function subtract(firstInteger, secondInteger){
+    return firstInteger - secondInteger;
+}
+
+function multiply(firstInteger, secondInteger){
+    return firstInteger * secondInteger;
+}
+
+function divide(firstInteger, secondInteger){
+    return firstInteger / secondInteger;
+}
+
