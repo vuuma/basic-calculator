dc3fb2e1a6d6b7be5530fb3f002e49e2a528826b
Matthew <matthewvuu@gmail.com>
2023-01-13 22:37:46 +1100

    implement functionality for basic arithmetic and options (plus, minus, multiply, divide, clear, and equals).

--- js/javascript.js
+++ js/javascript.js
@@ -26,7 +26,7 @@ function operate(operator, firstInteger, secondInteger){
         return subtract(firstInteger, secondInteger);
     }
 
-    else if(operator == "*"){
+    else if(operator == "x"){
         return multiply(firstInteger, secondInteger);
     }
 
@@ -38,3 +38,67 @@ function operate(operator, firstInteger, secondInteger){
         return null;
     } 
 }
+
+function clearValues(){
+    firstIntegerArray.length = 0;
+    secondIntegerArray.length = 0;
+}
+
+function clearResults(){
+    operation.textContent = '';
+    result.textContent = '';
+}
+
+const buttons = document.querySelectorAll('button');
+const operation = document.querySelector('.operation');
+const result = document.querySelector('.result');
+const integerPattern = /^[0-9]$/;
+const operatorPattern = /[+-/x]/;
+const clearPattern = /C/;
+const equalPattern = /=/;
+let operator = '';
+let firstIntegerArray = [];
+let secondIntegerArray = [];
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        
+        if(integerPattern.test(button.textContent)){
+            if(operation.textContent.includes('-') || operation.textContent.includes('+') || operation.textContent.includes('/') || operation.textContent.includes('x')){
+                operation.appendChild(document.createTextNode(`${button.textContent}`));
+                secondIntegerArray.push(button.textContent);
+            }
+
+            else{
+                firstIntegerArray.push(button.textContent);
+                operation.appendChild(document.createTextNode(`${button.textContent}`));
+            }
+        }   
+
+        else if(operatorPattern.test(button.textContent)){
+            operator = button.textContent;
+            operation.appendChild(document.createTextNode(`${button.textContent}`)); 
+        }
+
+        else if(equalPattern.test(button.textContent)){
+            // Converts the Array of numbers into a single integer. i.e. [1, 0, 9] becomes 109.
+            let firstInteger = Number(firstIntegerArray.join(''));
+            let secondInteger = Number(secondIntegerArray.join(''));
+          
+            result.textContent = '';
+            let calculateResult = operate(operator, parseInt(firstInteger), parseInt(secondInteger));
+            result.appendChild(document.createTextNode(`${calculateResult}`));
+            clearValues()
+            operation.textContent = '';
+            operation.appendChild(document.createTextNode(`${calculateResult}`));
+            firstIntegerArray.push(calculateResult);
+        }
+
+        // Clears the screen when the C (clear button) is pressed.
+        else if(clearPattern.test(button.textContent)){
+            clearResults()
+            clearValues()
+        }
+    });
+});
