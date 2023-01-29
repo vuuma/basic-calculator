fd250b98d725afab3dd32f121df1cbaa043f473f
Matthew <matthewvuu@gmail.com>
2023-01-29 20:15:04 +1100

    add backspace feature and prevent multiple operators from appearing

--- js/javascript.js
+++ js/javascript.js
@@ -14,7 +14,6 @@ function divide(firstNumber, secondNumber){
     return firstNumber / secondNumber;
 }
 
-
 // The operate function takes in an operator sign and two numbers and parses it 
 // to the intended operation function (add, subtract, multiply, and divide).
 function operate(operator, firstNumber, secondNumber){
@@ -33,22 +32,27 @@ function operate(operator, firstNumber, secondNumber){
     else if(operator == "/"){
         return divide(firstNumber, secondNumber);
     }
-
-
 }
 
 function displayResult(){
-   
     let firstNumber = Number(firstNumberArray.join(''));
-    let secondNumber = Number(secondNumberArray.join(''));
-    
-    result.textContent = '';
-    let calculateResult = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
-    result.appendChild(document.createTextNode(`${calculateResult}`));
-    clearValues()
-    operation.textContent = '';
-    operation.appendChild(document.createTextNode(`${calculateResult}`));
-    firstNumberArray.push(calculateResult);
+
+    if(secondNumberArray.length == 0){
+        result.textContent = '';
+        result.appendChild(document.createTextNode(`${firstNumber}`));
+        
+    }
+    else{
+        let secondNumber = Number(secondNumberArray.join(''));
+        let calculateResult = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+
+        result.textContent = '';
+        result.appendChild(document.createTextNode(`${calculateResult}`));
+        clearValues()
+        operation.textContent = '';
+        operation.appendChild(document.createTextNode(`${calculateResult}`));
+        firstNumberArray.push(calculateResult);
+    }
 }
 
 function clearValues(){
@@ -69,8 +73,10 @@ function clearAllData(){
 const buttons = document.querySelectorAll('button');
 const operation = document.querySelector('.operation');
 const result = document.querySelector('.result');
+const backspace = document.querySelector('.backspace');
 const numberPattern = /^[0-9]$/;
 const operatorPattern = /[+-/x]/;
+const backspacePattern = /</;
 const clearPattern = /C/;
 const equalPattern = /=/;
 let operator = '';
@@ -78,11 +84,11 @@ let firstNumberArray = [];
 let secondNumberArray = [];
 let firstNumber;
 let secondNumber;
-
+let displayFirstNumber;
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        
+        //console.log(operation);
         if(numberPattern.test(button.textContent)){
             if(operation.textContent.includes('-') || operation.textContent.includes('+') || operation.textContent.includes('/') || operation.textContent.includes('x')){
                 operation.appendChild(document.createTextNode(`${button.textContent}`));
@@ -96,26 +102,19 @@ buttons.forEach((button) => {
         }   
 
         else if(operatorPattern.test(button.textContent)){
-            
-            
             if(firstNumberArray.length == 0 && button.textContent == '-'){
                 firstNumberArray.push(0);
                 operation.appendChild(document.createTextNode(0)); 
             }
             
             else if(secondNumberArray.length > 0 && (operation.textContent.includes('-') || operation.textContent.includes('+') || operation.textContent.includes('/') || operation.textContent.includes('x'))){
-            displayResult();
+                displayResult();
             }
 
-            else{
-                firstNumber = Number(firstNumberArray.join(''));
-                operation.textContent = '';
-                operation.appendChild(document.createTextNode(`${firstNumber}`));
+            if(operation.lastChild.textContent != '+' &&  operation.lastChild.textContent != '-' && operation.lastChild.textContent != '/' && operation.lastChild.textContent != 'x'){
+                operator = button.textContent;
+                operation.appendChild(document.createTextNode(`${button.textContent}`)); 
             }
-    
-
-            operator = button.textContent;
-            operation.appendChild(document.createTextNode(`${button.textContent}`)); 
         }
 
         else if(equalPattern.test(button.textContent)){
@@ -123,6 +122,28 @@ buttons.forEach((button) => {
             displayResult()
         }
 
+        else if(backspacePattern.test(button.textContent)){
+    
+            if(secondNumberArray.length > 0 && (operation.textContent.includes('-') || operation.textContent.includes('+') || operation.textContent.includes('/') || operation.textContent.includes('x'))){
+                secondNumberArray.pop();
+                operation.removeChild(operation.lastChild);    
+                console.log(secondNumberArray);    
+            }
+
+            else if(secondNumberArray.length == 0 && operator == ''){
+                
+                firstNumberArray.pop();
+                console.log(firstNumberArray);
+                console.log(displayFirstNumber);
+                operation.removeChild(operation.lastChild);  
+            }
+            
+            else if(operation.lastChild.textContent == '+' || operation.lastChild.textContent == '-' || operation.lastChild.textContent == '/' || operation.lastChild.textContent == 'x'){
+                operator = '';
+                operation.removeChild(operation.lastChild);  
+            }
+        }
+      
         // Clears the screen when the C (clear button) is pressed.
         else if(clearPattern.test(button.textContent)){
             clearAllData()
